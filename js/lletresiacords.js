11629a935fe07dc1df3b0b45de79a651c028be84
Ricard Viñas <ricard.vinasf@gmail.com>
2022-12-08 19:55:21 +0100

    Lletres i acords

---
+++ js/lletresiacords.js
@@ -0,0 +1,16 @@
+let menu0 = document.getElementById('menu4');
+
+var checkbox0 = document.getElementById('check4');
+
+let lletres = document.getElementById('lletres');
+
+checkbox0.addEventListener('change', function() {
+  if (this.checked) {
+    menu0.style.height = "1000px";
+    menu0.style.backgroundColor = "rgba(0, 0, 0, 0.92)";
+    lletres.style.overflowY = "hidden";
+  } else {
+    menu0.style.height = "fit-content";
+    menu0.style.backgroundColor = "rgba(0, 0, 0, 0)";
+  }
+});
