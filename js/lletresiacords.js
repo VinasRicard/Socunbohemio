c9e02f0cc32a9e514b52b2a26f1f1baa64763560
Ricard Viñas <ricard.vinasf@gmail.com>
2022-12-08 20:45:26 +0100

    Phone adjustments

--- js/lletresiacords.js
+++ js/lletresiacords.js
@@ -6,7 +6,7 @@ let lletres = document.getElementById('lletres');
 
 checkbox0.addEventListener('change', function() {
   if (this.checked) {
-    menu0.style.height = "1000px";
+    menu0.style.height = "2000px";
     menu0.style.backgroundColor = "rgba(0, 0, 0, 0.92)";
     lletres.style.overflowY = "hidden";
   } else {
