e68df2d8f16c7f817806512482c999003140df67
Ricard Viñas <ricard.vinasf@gmail.com>
2023-02-01 17:14:19 +0100

    Update concerts

--- firebase/js/concerts.js
+++ firebase/js/concerts.js
@@ -1,21 +1,32 @@
-let cont = document.getElementById('concerts_c');
-cont.className = 'fade';
+let page = document.getElementById('block1');
+page.className = 'fade';
 document.addEventListener("DOMContentLoaded", () => {
   window.setTimeout(function() {
-    cont.classList.remove('fade');
+    page.classList.remove('fade');
   }, 230);
 });
 
-let menu0 = document.getElementById('menu2');
+let page2 = document.getElementById('block2');
+page.className = 'fade';
+document.addEventListener("DOMContentLoaded", () => {
+  window.setTimeout(function() {
+    page2.classList.remove('fade');
+  }, 230);
+});
 
-var checkbox0 = document.getElementById('check2');
+let menu0 = document.getElementById('menu7');
 
-var barras = document.getElementById('barras2');
+var checkbox0 = document.getElementById('check4');
+
+let lletres = document.getElementById('lletres');
+
+var barras = document.getElementById('barras4');
 
 checkbox0.addEventListener('change', function() {
   if (this.checked) {
-    menu0.style.height = "100%";
+    menu0.style.height = "2000px";
     menu0.style.backgroundColor = "rgba(0, 0, 0, 0.92)";
+    lletres.style.overflowY = "hidden";
     barras.src = "../res/close.png";
   } else {
     menu0.style.height = "fit-content";
@@ -23,5 +34,3 @@ checkbox0.addEventListener('change', function() {
     barras.src = "../res/menu.png";
   }
 });
-
-
