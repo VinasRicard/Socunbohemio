6f456ad15d994a4aa4425b3576c84d24fe61048b
Ricard Viñas <ricard.vinasf@gmail.com>
2023-02-25 12:04:07 +0100

    Versio actual

--- firebase/js/concerts2.js
+++ firebase/js/concerts2.js
@@ -24,10 +24,10 @@ var barras = document.getElementById('barras4');
 
 checkbox0.addEventListener('change', function() {
   if (this.checked) {
+    barras.src = "../res/close.png";
     menu0.style.height = "2000px";
     menu0.style.backgroundColor = "rgba(0, 0, 0, 0.92)";
     lletres.style.overflowY = "hidden";
-    barras.src = "../res/close.png";
   } else {
     menu0.style.height = "fit-content";
     menu0.style.backgroundColor = "rgba(0, 0, 0, 0)";
